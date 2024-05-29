dc9d36d8a997d5cfb3e05627454aad67f5ff7e08
jujospring <jujospring@gmail.com>
2024-05-29 11:40:01 -0400

    Completed Learn NextJS course
    Chapters 15 and 16

--- nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import Form from "../../../../ui/invoices/edit-form";
 import Breadcrumbs from "../../../../ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
